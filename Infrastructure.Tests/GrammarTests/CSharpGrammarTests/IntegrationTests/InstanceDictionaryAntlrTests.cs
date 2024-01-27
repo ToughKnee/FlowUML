0f5a0d11310b7844941d8a0b0836e9c3dd8faa16
Carlos Antonio <CARLOS.sanchezblanco@ucr.ac.cr>
2024-01-27 15:32:30 -0600

    Implement relative paths for tInfrastructure Tests

--- Infrastructure.Tests/GrammarTests/CSharpGrammarTests/IntegrationTests/InstanceDictionaryAntlrTests.cs
+++ Infrastructure.Tests/GrammarTests/CSharpGrammarTests/IntegrationTests/InstanceDictionaryAntlrTests.cs
@@ -10,24 +10,24 @@ namespace Infrastructure.Tests.GrammarTests.CSharpGrammarTests.IntegrationTests
 {
     public class InstanceDictionaryAntlrTests
     {
-        private readonly string pathToTestFile1 = "C:\\Users\\Usuario\\source\\repos\\FlowUML\\Infrastructure.Tests\\GrammarTests\\CSharpGrammarTests\\IntegrationTests\\TestFiles\\TextFile1.txt";
+        private static string currentDirectoryPath = "..\\..\\..\\";
+        private readonly string pathToTestFile1 = "GrammarTests\\CSharpGrammarTests\\IntegrationTests\\TestFiles\\TextFile1.txt";
         private ANTLRService _antlrService;
         public InstanceDictionaryAntlrTests()
         {
         }
 
         [Fact]
-        public void Analyze_BasicmethodRule_instanceDictHasCorrectCount()
+        public void Analyze_BasicmethodRuleFromFile_InstancesDictHasCorrectCountOfElements()
         {
             // Arrange
-            _antlrService = new ANTLRService(pathToTestFile1, true);
+            _antlrService = new ANTLRService(currentDirectoryPath + pathToTestFile1, true);
 
             // Act
             _antlrService.RunVisitorWithSpecificStartingRule("method");
 
             // Assert
             Assert.True(InstancesDictionaryManager.instance.instancesDictionary.Count == 5);
-            
         }
     }
 }
